f9eafd85fcf53771f7916397e4a909cb0e064cb4
Agus Ekanurdi <agus@ecquaria.com>
2018-08-07 16:17:02 +0800

    Initial commit

---
+++ src/main/java/com/aguseka/concourse/ConcoursePocApplication.java
@@ -0,0 +1,12 @@
+package com.aguseka.concourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcoursePocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcoursePocApplication.class, args);
+    }
+}
